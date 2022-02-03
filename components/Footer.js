9dadfa15927243407864686938470830a97bb9c2
about7code <sanaullahhaifz@gmail.com>
2022-02-03 18:40:46 +0400

    link fixes

--- components/Footer.js
+++ components/Footer.js
@@ -6,25 +6,25 @@ const Footer = () => {
         <div className="container-footer">
             <ul className="social-btns">
             <li className="social-btn">
-                <a href="https://twitter.com/sana_is_BB" target="_blank"><i className="fab fa-twitter" ></i></a>
+                <a href="https://twitter.com/sana_is_BB" rel="noopener noreferrer" target="_blank"><i className="fab fa-twitter" ></i></a>
             </li>
             <li className="social-btn">
-                <a href="https://www.linkedin.com/in/sanaullah-hafiz-595a16b8" target="_blank"><i className="fab fa-linkedin-in" ></i></a>
+                <a href="https://www.linkedin.com/in/sanaullah-hafiz-595a16b8" rel="noopener noreferrer" target="_blank"><i className="fab fa-linkedin-in" ></i></a>
             </li>
             <li className="social-btn">
-                <a href="https://open.spotify.com/user/sandwich5656" target="_blank"><i className="fab fa-spotify" ></i></a>
+                <a href="https://open.spotify.com/user/sandwich5656" rel="noopener noreferrer" target="_blank"><i className="fab fa-spotify" ></i></a>
             </li>
             <li className="social-btn">
-                <a href="https://codepen.io/about7codes" target="_blank"><i className="fab fa-codepen" ></i></a>
+                <a href="https://codepen.io/about7codes" rel="noopener noreferrer" target="_blank"><i className="fab fa-codepen" ></i></a>
             </li>
             <li className="social-btn">
-                <a href="https://github.com/about7codes" target="_blank"><i className="fab fa-github" ></i></a>
+                <a href="https://github.com/about7codes" rel="noopener noreferrer" target="_blank"><i className="fab fa-github" ></i></a>
             </li>
             </ul>
 
             <div className="to-top">
             <a className="rocket" href="#intro">
-                <img src="/earth.png" alt="ico" /> <br />
+                {/* <img src="/earth.png" alt="ico" /> <br /> */}
                 <p id="trick">Beam me up, Scotty!</p>
             </a>
             </div>
